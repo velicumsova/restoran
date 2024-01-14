6cc99412f56743d8082240132261a94a0d76ba10
velicumsova <120342904+velicumsova@users.noreply.github.com>
2024-01-14 23:20:37 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
+// menu wrapping
 const burger = document.querySelector('#burger');
-const menu= document.querySelector('#menu');
+const menu = document.querySelector('#menu');
 burger.addEventListener('click', () => {
     menu.classList.toggle('disp');
+});
+
+// header moving
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.getElementById('movingHeader');
+
+    document.addEventListener('mousemove', function (e) {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+
+        const offsetX = (window.innerWidth / 2 - mouseX) / 10;
+        const offsetY = (window.innerHeight / 2 - mouseY) / 10;
+
+        header.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+    });
 });
